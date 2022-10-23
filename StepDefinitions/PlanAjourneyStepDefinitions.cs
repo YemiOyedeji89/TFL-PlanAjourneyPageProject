f5773d0e644312a5bfd26ea20ab98071cfcbd645
yemi.oyedeji89@gmail.com <yemi.oyedeji89@gmail.com>
2022-10-23 01:23:29 +0100

    pages rearranged

--- StepDefinitions/PlanAjourneyStepDefinitions.cs
+++ StepDefinitions/PlanAjourneyStepDefinitions.cs
@@ -1,3 +1,4 @@
+using BoDi;
 using Docker.DotNet.Models;
 using NUnit.Framework;
 using OpenQA.Selenium;
@@ -9,81 +10,100 @@ using TechTalk.SpecFlow.Assist;
 namespace PlanAjourneyPageProject.StepDefinitions
 {
     [Binding]
-    public class PlanAjourneyStepDefinitions 
+    public class PlanAjourneyStepDefinitions
     {
+       
         IWebDriver? driver;
+        public PlanAjourneyStepDefinitions(IWebDriver? _driver)
+        {
+            this.driver = _driver;
 
-        PlanAJourneyHomePage planAJourney = new PlanAJourneyHomePage();
-
+        }
+       // PlanAJourneyHomePage planAJourney = new PlanAJourneyHomePage();
+     
 
         [Given(@"that I am on the tfl plan a journey page")]
         public void GivenThatIAmOnTheTflPlanAJourneyPage()
         {
-            planAJourney.PlanAjourneyPage();
+            Tfl_HomePage TflHomepg = new Tfl_HomePage(driver);
+            TflHomepg.PlanAjourneyPage();
         }
 
         [When(@"I enter location")]
         [Obsolete]
         public void WhenIEnterLocation(Table table)
         {
-            planAJourney.SearchBoxFromTo(table.Rows[0]["From"], table.Rows[0]["To"]);
+            PlanAJourneyWidget SearchFromToTxtBox = new PlanAJourneyWidget(driver);
+
+            SearchFromToTxtBox.SearchBoxFromTo(table.Rows[0]["From"], table.Rows[0]["To"]);
         }
 
         [When(@"I select the date and time")]
        
         public void WhenISelectTheDate(Table table)
         {
-            planAJourney.SelectDateAndTime(table.Rows[0]["Date"], table.Rows[0]["Time"]);
+            PlanAJourneyWidget DateTimeSelected = new PlanAJourneyWidget(driver);
+            DateTimeSelected.SelectDateAndTime(table.Rows[0]["Date"], table.Rows[0]["Time"]);
         }
 
         [When(@"I click Trun on recent journey")]
         public void WhenIHaveClickTrunOnRecentJourney()
         {
+            PlanAJourneyHomePage planAJourney = new PlanAJourneyHomePage(driver);
             planAJourney.TurnOnRecentJourney();
         }
 
         [When(@"I set the time leaving now")]
         public void WhenISetTheTimeLeavingNow()
         {
-            planAJourney.LeavingNow();
+            PlanAJourneyWidget SelectLeavingNow = new PlanAJourneyWidget(driver);
+            SelectLeavingNow.LeavingNow();
         }
 
         [When(@"I click on plan my journey tab")]
         public void WhenIClickOnPlanMyJourneyTab()
         {
-            planAJourney.PlanMyJourneyTab();
+            PlanAJourneyWidget PlanJourneyTab = new PlanAJourneyWidget(driver);
+            PlanJourneyTab.PlanMyJourneyTab();
         }
 
         [Then(@"I should be taken to the journey result page")]
         [Obsolete]
         public void ThenIShouldBeTakenToTheJourneyResultPage(Table table)
         {
-            planAJourney.JourneyResultPage(table.Rows[0]["From"],table.Rows[0]["To"]);
+            JourneyResultPage JourneyResultPageSection = new JourneyResultPage(driver);
+            JourneyResultPageSection.JourneyResultPagePlan(table.Rows[0]["From"],table.Rows[0]["To"]);
         }
 
         [Then(@"I should be taken to the journey result date and time planned")]
         [Obsolete]
         public void ThenIShouldBeTakenToTheJourneyResultDateAndTimePlanned(Table table)
         {
-            planAJourney.JourneyTimePlanned(table.Rows[0]["DateAndTime"]);
+            JourneyResultPage JourneyResultTimePlanned = new JourneyResultPage(driver);
+            JourneyResultTimePlanned.JourneyTimePlanned(table.Rows[0]["DateAndTime"]);
         }
 
         [When(@"I am on the Journey result page")]
         [Obsolete]
         public void WhenIAmOnTheJourneyResultPage(Table table)
         {
-            planAJourney.JourneyTimePlanned(table.Rows[0]["DateAndTime"]);
+            JourneyResultPage JourneyTime = new JourneyResultPage(driver);
+            JourneyTime.JourneyTimePlanned(table.Rows[0]["DateAndTime"]);
         }
 
         [When(@"I navigated back to the tfl plan a journey page")]
+        [Obsolete]
         public void WhenINavigatedBackToTheTflPlanAJourneyPage()
         {
-            planAJourney.NavigateBackToJourneyPlannerPage();
+            JourneyResultPage NavigateToJourneyPlanPg = new JourneyResultPage(driver);
+            NavigateToJourneyPlanPg.NavigateBackToJourneyPlannerPage();
         }
 
         [Then(@"I can see my journey planned in the Rescent tab")]
+        [Obsolete]
         public void ThenIAnSeeMyJourneyPlannedInTheRescentTab(Table table)
         {
+            PlanAJourneyHomePage planAJourney = new PlanAJourneyHomePage(driver);
             planAJourney.RecentJourneyTabDisplayed(table.Rows[0]["RecentPlan"]);
         }
 
@@ -91,74 +111,87 @@ namespace PlanAjourneyPageProject.StepDefinitions
         [Obsolete]
         public void WhenIClickEditJourney()
         {
-            planAJourney.EditJourney();
+            JourneyResultPage EditJourneyLink = new JourneyResultPage(driver);
+            EditJourneyLink.EditJourney();
         }
 
         [When(@"I updated the location")]
         public void WhenIUpdatedTheLocation(Table table)
         {
-            planAJourney.EditLocation(table.Rows[0]["From"], table.Rows[0]["To"]);
+            EditJourneyPage EditLocationPage = new EditJourneyPage(driver);
+            EditLocationPage.EditLocation(table.Rows[0]["From"], table.Rows[0]["To"]);
         }
 
         [When(@"I click arriving")]
         public void WhenIClickArriving()
         {
-            planAJourney.ArrivingBox();
+            EditJourneyPage SelectArriving = new EditJourneyPage(driver);
+            SelectArriving.ArrivingBox();
         }
 
         [When(@"I updated the date and time")]
         public void WhenIUpdatedTheDateAndTime(Table table)
         {
-            planAJourney.UpdatedTheDateAndTime(table.Rows[0]["Date"], table.Rows[0]["Time"]);
+            EditJourneyPage UpdateDateTime = new EditJourneyPage(driver);
+            UpdateDateTime.UpdatedTheDateAndTime(table.Rows[0]["Date"], table.Rows[0]["Time"]);
         }
 
         [When(@"I click on update journey tab")]
         public void WhenIClickOnUpdateJourneyTab()
         {
-            planAJourney.ClickUpdateJourneyButton();
+            EditJourneyPage UpdatejourneyBtn = new EditJourneyPage(driver);
+            UpdatejourneyBtn.ClickUpdateJourneyButton();
         }
         
         [Then(@"I can see my planned journey updated in the Juorney Result Page")]
+        [Obsolete]
         public void ThenICanSeeMyPlannedJourneyUpdatedInTheJuorneyResultPage(Table table)
         {
-            planAJourney.UpdatedJourneyResultPage(table.Rows[0]["From"], table.Rows[0]["To"], table.Rows[0]["DateAndTime"]);
+            JourneyResultPage UpdateResult = new JourneyResultPage(driver);
+            UpdateResult.UpdatedJourneyResultPage(table.Rows[0]["From"], table.Rows[0]["To"], table.Rows[0]["DateAndTime"]);
         }
-
+         
         [When(@"I enter location  from ""([^""]*)"" and location to ""([^""]*)""")]
         [Obsolete]
         public void WhenIEnterLocationFromAndLocationTo(string From, string To)
         {
-            planAJourney.SearchBoxFromTo(From, To);
+            PlanAJourneyWidget FromToFields = new PlanAJourneyWidget(driver);
+            FromToFields.SearchBoxFromTo(From, To);
         }
 
         [When(@"I set the date ""([^""]*)"" and time ""([^""]*)""")]
         public void WhenISetTheDateAndTime(string Date, string Time)
         {
-            planAJourney.SelectDateAndTime(Date, Time);
+            PlanAJourneyWidget DateTimeSelect = new PlanAJourneyWidget(driver);
+            DateTimeSelect.SelectDateAndTime(Date, Time);
         }
 
         [Then(@"I should be taken to the journey result page section with location  from ""([^""]*)"" and location to ""([^""]*)""")]
         public void ThenIShouldBeTakenToTheJourneyResultPageSectionWithLocationFromAndLocationTo(string From, string To)
         {
-            planAJourney.JourneyResulPageSection(From, To);
+            JourneyResultPage ResultPageSection = new JourneyResultPage(driver);
+            ResultPageSection.JourneyResulPageSection(From, To);
         }
 
         [Then(@"I should get an errorMessage ""([^""]*)""")]
         public void ThenIShouldGetAnErrorMessage(string ErrorMessage)
         {
-            planAJourney.ErrorMessageDisplayed(ErrorMessage);
+            PlanAJourneyWidget ErrorMessages = new PlanAJourneyWidget(driver);
+            ErrorMessages.ErrorMessageDisplayed(ErrorMessage);
         }
 
         [When(@"I enter from ""([^""]*)"" and location to ""([^""]*)""")]
         public void WhenIEnterFromAndLocationTo(string To, string From)
         {
-            planAJourney.BlankFromAndTo(From,To);
+            PlanAJourneyWidget BlankFields = new PlanAJourneyWidget(driver);
+            BlankFields.BlankFromAndTo(From,To);
         }
 
         [Then(@"I should get field error message from ""([^""]*)"" and field error message to ""([^""]*)""")]
         public void ThenIShouldGetFieldErrorMessageFromAndFieldErrorMessageTo(string FieldErrorMessageFrom, string FieldErrorMessageTo)
         {
-            planAJourney.FromToFieldValidationError(FieldErrorMessageFrom, FieldErrorMessageTo);
+            PlanAJourneyWidget FieldValidationError = new PlanAJourneyWidget(driver);
+            FieldValidationError.FromToFieldValidationError(FieldErrorMessageFrom, FieldErrorMessageTo);
         }
 
     }
